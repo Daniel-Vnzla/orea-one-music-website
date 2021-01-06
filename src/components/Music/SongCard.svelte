b5a5b5b33cd655c359a4b16cda5e2426e70159d7
unknown <rune.escape888@gmail.com>
2021-01-06 16:27:24 -0400

    add: SongCard component and some stuff

---
+++ src/components/Music/SongCard.svelte
@@ -0,0 +1,87 @@
+<script>
+	import Youtube from '../../icons/redes/youtube-with-circle.svg';
+	import Spotify from '../../icons/redes/spotify.svg';
+	import Itunes from '../../icons/redes/itunes.svg';
+</script>
+
+<div class="song-card">
+	<div class="song-img">
+		<img src="/images/explicit.jpg" alt="explica">
+	</div>
+	<div class="song-info">
+		<h4 class="song-info-title">Sangre Roja</h4>
+		<p class="song-info-author">Orea One ft. Sharly la voz</p>
+	</div>
+	<div class="song-links">
+		<a href="/" target="_blank" data-tooltip="iTunes"> 
+			<Itunes width="100%" height="100%" fill="var(--secondary-color)" />
+		</a>	
+		<a href="/" target="_blank" data-tooltip="Spotify"> 
+			<Spotify width="100%" height="100%" fill="var(--secondary-color)" />
+		</a>	
+		<a href="/" target="_blank" data-tooltip="Youtube"> 
+			<Youtube width="100%" height="100%" fill="var(--secondary-color)" />
+		</a>	
+	</div>
+</div>
+
+<style>
+.song-card {
+	display: grid;
+	grid-template-columns: 10% 62% 28%;
+	border-radius: 2px;
+}
+
+.song-img img {
+	height: 100%;
+}
+
+.song-info {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	background-color: #5a5a5a;
+}
+
+.song-info-title {
+	font-size: 1.8rem;
+}
+
+.song-info-author {
+	font-size: 1rem;
+}
+
+.song-links {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: var(--primary-color);
+}
+
+.song-links a {
+	position: relative;
+	width: 60%;
+	height: 60%;
+}
+
+.song-links a::before {
+	position: absolute;
+	top: 0;
+	left: 50%;
+	content: attr(data-tooltip);
+	background-color: #5a5a5a;
+	color: var(--text-color);
+	padding: 4px 16px;
+	border-radius: 4px;
+	opacity: 0;
+	visibility: hidden;
+	transform: translate(-50%, -220%);
+	transition: opacity var(--transition-speed);
+}
+
+.song-links a:hover::before {
+	opacity: 1;
+	visibility: visible;
+}
+	
+</style>
--- src/routes/Music.svelte
+++ src/routes/Music.svelte
@@ -1,1 +1,60 @@
-<h2>Music</h2>
+<script>
+	import OreaOneTag from '../commun/OreaOneTag.svelte';
+	import SongCard from '../components/Music/SongCard.svelte';
+
+</script>
+
+<section class="music">
+	<OreaOneTag />
+	<div class="music-wrapper">
+		<h2 class="music-title">Música</h2>
+		<p class="music-legend">Escucha lo ultimo de Orea One</p>
+		<div class="songs">
+			<h3 class="songs-title">Toda la música</h3>
+			<SongCard />
+			<SongCard />
+			<SongCard />
+			<SongCard />
+			<SongCard />
+			<SongCard />
+			<SongCard />
+			<SongCard />
+		</div>
+	</div>
+</section>
+
+<style>
+	.music {
+		position: relative;
+		overflow: hidden;
+		padding-top: 3rem;
+	}
+
+	.music-wrapper {
+		position: relative;
+		width: 90%;
+		max-width: 1400px;
+		margin: 0 auto;
+	}
+
+	.music-title {
+		font-family: "Road rage";
+	}
+
+	.music-legend {
+		color: #ccc;
+		font-size: 1.5rem;
+	}
+
+
+	.songs {
+		margin-top: 5rem;
+		display: grid;
+		grid-gap: 20px;
+	}
+
+	.songs-title {
+		margin-bottom: 1rem;
+	}
+
+</style>	
