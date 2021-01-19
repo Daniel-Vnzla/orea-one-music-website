9fc246e89a6215e611dbc9fe18cf24e5f3bd7aa0
unknown <rune.escape888@gmail.com>
2021-01-19 18:16:43 -0400

    add: anime js library

---
+++ src/assets/anime.js
@@ -0,0 +1,45 @@
+import anime from 'animejs';
+
+const parameters = {
+	duration: 1500,
+	easing: "easeInBack",
+}
+const { duration, easing } = parameters;
+
+export const homeAnime =() => {
+	anime.timeline({
+		duration
+	})
+	.add({
+		targets: "#anime-fade-top-text",
+	  translateY: [-50, 0],
+	  opacity: [0, 1],
+	  easing
+	})
+	.add({
+		targets: "#anime-redes-home .icon",
+	  translateY: [-50, 0],
+	  opacity: [0, 1],
+	  delay: anime.stagger(100, { easing: 'easeOutQuad' }),
+	});
+
+	anime({
+		targets: "#anime-fade-bottom-text",
+		translateY: [50, 0],
+	  opacity: [0, 1],
+	  easing,
+	  duration,
+	});
+}
+
+export const animeLazyLoadingImg = ({ target }) => {
+	target.style.opacity = 1;
+
+	anime({
+		targets: target,
+	  opacity: [0, 1],
+	  scale: [2, 1],
+	  easing: "easeOutQuad",
+	  duration: 33500,
+	})
+}
